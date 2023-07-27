eb8328ebf7083c18321a47a23d6a58c071db9080
moyu <279972161@qq.com>
2023-07-28 00:39:05 +0800

    update

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,5 +2,5 @@
 
 # 执行初始化环境的脚本
 $HOME/work/scripts/init_env.sh &
-$HOME/work/scripts/init_container_env.sh 53443536-6c1b-46bb-8583-919066bc8111 &
+$HOME/work/scripts/init_container_env.sh 0f81ad28-f85e-48e5-bc31-47072822eefd &
 wait
--- scripts/run_dev_container.sh
+++ scripts/run_dev_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # 启动开发容器,将本机GPU挂载进去,ipc=host是保证进程间通信时所使用的共享内存空间足够,详见pytorch仓库readme
-docker run --gpus all -itd --ipc=host --name my-dev-env moyusir233/my-disributed-ml-develop:latest
+docker run --gpus all -itd --ipc=host --name my-dev-env moyusir233/my-disributed-ml-develop:new
