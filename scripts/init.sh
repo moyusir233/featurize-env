e050e8c3057986a5a931e6633348c095d2ac32c9
moyu <279972161@qq.com>
2023-08-12 10:00:32 +0800

    update

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,5 +2,5 @@
 
 # 执行初始化环境的脚本
 $HOME/work/scripts/init_env.sh &
-$HOME/work/scripts/init_container_env.sh &
+$HOME/work/scripts/init_container_env.sh 399a507b-6ecf-4b29-b3d6-88a91cb05736 &
 wait
--- scripts/init_container_env.sh
+++ scripts/init_container_env.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
 
-# if (( $# != 1 )); then
-#     echo "missing dataset id!"
-#     exit 1
-# fi
+if (( $# != 1 )); then
+    echo "missing dataset id!"
+    exit 1
+fi
 
-# {
-# # 下载与导入开发环境的镜像
-# featurize dataset download $1 && sleep 10s && docker load -i $HOME/data/my-dev-env2.tar
-# }&
+{
+# 下载与导入开发环境的镜像
+featurize dataset download $1 && sleep 10s && docker load -i $HOME/data/my-dev-env.tar
+}&
 
 # 初始化容器运行环境
 # 安装nvidia的容器运行时
