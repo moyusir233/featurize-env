722aaca7ea10b039dc1991c3cc561218dd849ce3
moyu <279972161@qq.com>
2023-08-12 19:59:56 +0800

    update

--- .on_boot.sh
+++
@@ -1,5 +0,0 @@
-sudo -i -u featurize bash << EOF
-
-# 执行初始化环境的脚本
-$HOME/work/scrpts/init.sh
-EOF
--- scripts/init.sh
+++ scripts/init.sh
@@ -4,3 +4,9 @@
 $HOME/work/scripts/init_env.sh &
 $HOME/work/scripts/init_container_env.sh 399a507b-6ecf-4b29-b3d6-88a91cb05736 &
 wait
+
+# 拉取git仓库与配置git
+git config --global user.name moyu
+git config --global user.email 279972161@qq.com
+cd $HOME && git clone -b develop git@github.com:moyusir233/tch-rs.git 
+cd $HOME && git clone -b torchv2.0.1_dev --depth 1 git@github.com:moyusir233/pytorch.git 
